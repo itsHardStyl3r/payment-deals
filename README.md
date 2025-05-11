2a1158ef73b57ec8ca98c1287a2c99716acac3a4
Aleksander Gencel <johnnylittleplanet@gmail.com>
2025-05-11 21:18:08 +0200

    payments: Create a sketch of the program and a README

--- README.md
+++ README.md
@@ -1,3 +1,98 @@
 # Payment deals
 
-This project uses Java 21.
+This is a simple Java program that shows the best payment method for a given orders based on the provided payment methods and their discounts.
+
+## Requirements
+
+⚠️ You need Java 21 to run this program.
+
+You need two files, one with the orders and the other one with payment methods.
+Both of them are supposed to be json files, e.g.
+
+- orders.json
+
+```json
+[
+  {
+    "id": "ORDER1",
+    "value": "100.00",
+    "promotions": [
+      "mZysk"
+    ]
+  },
+  {
+    "id": "ORDER2",
+    "value": "200.00",
+    "promotions": [
+      "BosBankrut"
+    ]
+  },
+  {
+    "id": "ORDER3",
+    "value": "150.00",
+    "promotions": [
+      "mZysk",
+      "BosBankrut"
+    ]
+  },
+  {
+    "id": "ORDER4",
+    "value": "50.00"
+  }
+]
+```
+
+- paymentmethods.json
+
+```json
+[
+  {
+    "id": "PUNKTY",
+    "discount": "15",
+    "limit": "100.00"
+  },
+  {
+    "id": "mZysk",
+    "discount": "10",
+    "limit": "180.00"
+  },
+  {
+    "id": "BosBankrut",
+    "discount": "5",
+    "limit": "200.00"
+  }
+]
+```
+
+## Building
+
+1. Clone the repository
+2. Open the terminal and navigate to the project directory
+3. Run the following command to build the project:
+   ```bash
+   mvn install
+   ```
+4. This will create a JAR file in the `target` directory.
+
+## Running the program
+
+You either need to build the program first, or download the JAR file from the releases. Run the following command to
+execute the program:
+
+```
+java -jar payment-deals-1.0-SNAPSHOT.jar <path_to_orders.json> <path_to_paymentmethods.json>
+```
+
+## Testing
+
+To run the tests, use the following command:
+
+```bash
+mvn test
+```
+
+and optionally, you can build a website with a raport and test results:
+
+```bash
+mvn site
+```
--- src/main/java/me/hardstyl3r/payments/Main.java
+++ src/main/java/me/hardstyl3r/payments/Main.java
@@ -1,7 +1,43 @@
 package me.hardstyl3r.payments;
 
+import me.hardstyl3r.payments.adapters.OrderAdapter;
+import me.hardstyl3r.payments.adapters.PayMethodAdapter;
+import me.hardstyl3r.payments.managers.FileManager;
+import me.hardstyl3r.payments.objects.Order;
+import me.hardstyl3r.payments.objects.PayMethod;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Promocje dla metod płatności.");
+        if (args.length != 2) {
+            System.out.println("Correct usage: java -jar <jar-file> <orders-file> <payment-methods-file>.");
+            return;
+        }
+        FileManager<Order> orderManager = new FileManager<>(Order.class, new OrderAdapter());
+        FileManager<PayMethod> payMethodManager = new FileManager<>(PayMethod.class, new PayMethodAdapter());
+        String ordersJson = "", payMethodsJson = "";
+        try {
+            ordersJson = Files.readString(Paths.get(args[0]));
+        } catch (Exception e) {
+            System.out.println("Error reading orders file: " + e.getMessage() + ".");
+        }
+        try {
+            payMethodsJson = Files.readString(Paths.get(args[1]));
+        } catch (Exception e) {
+            System.out.println("Error reading payment methods file: " + e.getMessage() + ".");
+        }
+
+        Set<Order> orders = orderManager.getFromJson(ordersJson);
+        for (Order order : orders)
+            System.out.println("Order ID: " + order.getId() + "," +
+                    "Value: " + order.getValue() + ", Promotions: " + order.getPromotions());
+
+        Set<PayMethod> payMethods = payMethodManager.getFromJson(payMethodsJson);
+        for (PayMethod payMethod : payMethods)
+            System.out.println("Payment Method ID: " + payMethod.getId() + "," +
+                    "Discount: " + payMethod.getDiscount() + "," + "Limit: " + payMethod.getLimit());
     }
 }
