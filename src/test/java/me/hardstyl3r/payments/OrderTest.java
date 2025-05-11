efed3411a31219fc9459ee826461151f8eb90049
Aleksander Gencel <johnnylittleplanet@gmail.com>
2025-05-11 21:03:52 +0200

    payments: objects: Thoroughly test equals() for more coverage

--- src/test/java/me/hardstyl3r/payments/OrderTest.java
+++ src/test/java/me/hardstyl3r/payments/OrderTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderTest {
 
@@ -21,5 +20,18 @@ public class OrderTest {
         assertEquals("ORDER4", order2.getId());
         assertEquals(50, order2.getValue());
         assertNull(order2.getPromotions());
+    }
+
+    @SuppressWarnings({"EqualsWithItself", "SimplifiableAssertion", "EqualsBetweenInconvertibleTypes", "ConstantValue"})
+    @Test
+    public void testOrderEquals() {
+        Order order1 = new Order("ORDER1", 100.0, List.of("mZysk"));
+        Order order2 = new Order("ORDER1", 100.0, List.of("mZysk"));
+
+        assertEquals(order1, order1);
+        assertEquals(order1, order2);
+        assertNotSame(order1, order2);
+        assertFalse(order1.equals(""));
+        assertFalse(order2.equals(null));
     }
 }
--- src/test/java/me/hardstyl3r/payments/PayMethodTest.java
+++ src/test/java/me/hardstyl3r/payments/PayMethodTest.java
@@ -3,7 +3,7 @@ package me.hardstyl3r.payments;
 import me.hardstyl3r.payments.objects.PayMethod;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PayMethodTest {
 
@@ -18,5 +18,18 @@ public class PayMethodTest {
         assertEquals("mZysk", payMethod2.getId());
         assertEquals(10, payMethod2.getDiscount());
         assertEquals(180.0, payMethod2.getLimit());
+    }
+
+    @SuppressWarnings({"EqualsWithItself", "EqualsBetweenInconvertibleTypes", "SimplifiableAssertion", "ConstantValue"})
+    @Test
+    public void testPayMethodEquals() {
+        PayMethod payMethod1 = new PayMethod("PUNKTY", 15, 100.0);
+        PayMethod payMethod2 = new PayMethod("PUNKTY", 15, 100.0);
+
+        assertEquals(payMethod1, payMethod1);
+        assertEquals(payMethod1, payMethod2);
+        assertNotSame(payMethod1, payMethod2);
+        assertFalse(payMethod1.equals(""));
+        assertFalse(payMethod2.equals(null));
     }
 }
